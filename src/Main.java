a4d71e0b6c1959ccb370a93677ce3e569dc7aadf
emrekabakci <emrekabakci.2@icloud.com>
2023-12-17 23:20:35 +0300

    Comment added.

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        //Variables defined.
         int n, r, z,p=1,d=1,s=1,result;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter 2 values for the combination calculation. C(n,r)");
 
-        System.out.print("First value c : ");
+        //Data retrieved from user.
+        System.out.print("First value n : ");
         n = scanner.nextInt();
 
         System.out.print("Second value r :");
@@ -15,6 +17,7 @@ public class Main {
 
         z = n - r;
 
+        //Factorial calculated.
         for (int i = 1; i <= n; i++) {
             p=p*i;
         }System.out.println("n! ="+p);
@@ -27,6 +30,8 @@ public class Main {
             s =s* a;
         }
         result = p/(d*s);
+
+        //Result printed.
         System.out.print("C(n,r) = " + result);
     }
 }
